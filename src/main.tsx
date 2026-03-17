66a34cace1e5947d9de65f28c74c05f65a0bc373
谦兰君 <hewun@lyceum.cc>
2026-03-17 19:46:16 +0800

    feat: disable Right-Click Menu and Prevent Selection

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// Disable global context menu (right-click)
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
