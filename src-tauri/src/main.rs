04f51a2c0c041b87c80bae19785cdc0e4bd2e28e
谦兰君 <hewun@lyceum.cc>
2026-03-20 00:10:54 +0800

    feat: add webview2 flags

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(not(debug_assertions))]
+    std::env::set_var(
+            "WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS",
+            "--disable-features=msWebOOUI,msPdfOOUI,msEdgeTranslate,msSmartScreen,msEdgeHistoryImport,msEdgeCollections,msEdgeShopping,msEdgeSidebar,msEdgeWritingAssist,msEdgeHub \
+             --disable-extensions \
+             --disable-component-update \
+             --disable-background-networking \
+             --disable-sync \
+             --metrics-recording-only \
+             --no-sandbox \
+             --no-first-run \
+             --no-default-browser-check \
+             --disable-logging"
+        );
     hellcall_desktop_lib::run()
 }
